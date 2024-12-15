095c5f760a4238c90b39ea25042ea3bf0b9b018d
etbcf <tarik.edu2@gmail.com>
2024-12-15 02:42:44 +0000

    Manage to make vimwiki work with markdown

--- .config/nvim/lua/user/vimwiki.lua
+++ .config/nvim/lua/user/vimwiki.lua
@@ -1,22 +1,16 @@
-local M = {
+return {
+  {
     "vimwiki/vimwiki",
-    lazy = false, -- Carrega automaticamente no início
+    init = function()
+      vim.g.vimwiki_list = {
+        {
+          path = "~/vimwiki",   -- Directory for your notes
+          syntax = "markdown",  -- Use Markdown syntax
+          ext = ".md",          -- File extension
+        },
+      }
+    end,
+  },
 }
 
-M.config = function()
-    -- Configuração básica do Vimwiki
-    vim.g.vimwiki_list = {
-        {
-            path = "~/vimwiki",    -- Diretório para suas notas
-            syntax = "markdown",  -- Usar Markdown
-            ext = ".md",          -- Extensão para os arquivos
-        },
-    }
-
-    vim.g.vimwiki_global_ext = 0 -- Impedir que outros arquivos .md sejam tratados como Vimwiki
-    vim.g.vimwiki_hl_headers = 1 -- Destacar cabeçalhos
-    vim.g.vimwiki_auto_chdir = 1 -- Alterar para o diretório do wiki automaticamente
-end
-
-return M
 
